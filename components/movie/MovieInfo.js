366d64f7d5cc291e885ec6e3d5225e48df004ee3
Moinul Haq <moinul192000@gmail.com>
2021-08-07 04:52:51 +0600

    Movie View Page Fix

--- components/movie/MovieInfo.js
+++ components/movie/MovieInfo.js
@@ -1,19 +1,19 @@
-import { Descriptions, Badge } from 'antd';
+import { Descriptions } from 'antd';
 
 function MovieInfo(props) {
 
     const { movie } = props;
     
     return (
-      <Descriptions title="Movie Info" bordered size="middle">
+      <Descriptions title="Movie Info" layout="vertical" bordered size="middle" labelStyle={{textAlign:"center",fontWeight:"bold"}}>
         <Descriptions.Item label="Title">{movie.original_title}</Descriptions.Item>
-        <Descriptions.Item label="release_date">{movie.release_date}</Descriptions.Item>
-        <Descriptions.Item label="revenue">{movie.revenue}</Descriptions.Item>
-        <Descriptions.Item label="runtime">{movie.runtime}</Descriptions.Item>
-        <Descriptions.Item label="vote_average" span={2}>{movie.vote_average}</Descriptions.Item>
-        <Descriptions.Item label="vote_count">{movie.vote_count}</Descriptions.Item>
-        <Descriptions.Item label="status">{movie.status}</Descriptions.Item>
-        <Descriptions.Item label="popularity">{movie.popularity}</Descriptions.Item>
+        <Descriptions.Item label="Release Date" span={2}>{movie.release_date}</Descriptions.Item>
+        <Descriptions.Item label="Revenue">{movie.revenue}</Descriptions.Item>
+        <Descriptions.Item label="Runtime">{movie.runtime}</Descriptions.Item>
+        <Descriptions.Item label="Vote Average">{movie.vote_average}</Descriptions.Item>
+        <Descriptions.Item label="Vote Count">{movie.vote_count}</Descriptions.Item>
+        <Descriptions.Item label="Status">{movie.status}</Descriptions.Item>
+        <Descriptions.Item label="Popularity">{movie.popularity}</Descriptions.Item>
       </Descriptions>
     )
 }
